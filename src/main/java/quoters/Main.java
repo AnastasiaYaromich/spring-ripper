55b253889a690da0797eed5986b13d1b8f3c94d6
JYaromich <jane19937788@gmail.com>
2023-11-23 23:43:32 +0300

    Part2

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -1,11 +1,14 @@
 package quoters;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         context.getBean(Quoter.class).sayQuote();
+
     }
 }
 
